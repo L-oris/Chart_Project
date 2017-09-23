3da4995945420137c14e154e83805159cb7fa1fb
Loris <loris4526@gmail.com>
2017-09-23 09:15:44 +0200

    Correctly upload csv tables to AWS S3

--- express/RESTfulRouter.js
+++ express/RESTfulRouter.js
@@ -1,11 +1,27 @@
 const express = require('express'),
       router = express.Router()
 
+const {readCsvFile} = require('../csv/methods')
 const {uploader,uploadToS3} = require('./middlewares')
 
 
+//UPLOAD CSV TABLE TO AWS S3
+router.post('/api/upload_table',uploader.single('file'),uploadToS3,function(req,res,next){
+  const {name,description} = req.body
+  const {filename} = req.file
+  console.log('file saved');
+  res.json({success:true})
+  // addTable(name,description,filename)
+  // .then(function(){
+  //   res.json({success:true})
+  // })
+  // .catch(function(err){
+  //   next('error happened adding new table into database')
+  // })
+})
+
+
 //SEND BACK CSV TABLE CONVERTED TO JSON
-const {readCsvFile} = require('../csv/methods')
 router.get('/api/get_table',function(req,res,next){
   readCsvFile('./dataSheets/mock_data.csv')
   .then(function(jsonTable){
@@ -15,6 +31,7 @@ router.get('/api/get_table',function(req,res,next){
     next(`Error converting csv table  to json`)
   })
 })
+
 
 //SEND BACK MOCK DATA TO DISPLAY
 router.get('/api/get_data',function(req,res,next){
